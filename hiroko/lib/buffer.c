3b5ef194dc4af164f7682bb054bc42c7d93d4e3f
asmateus <mateus@uninorte.edu.co>
2017-11-19 15:53:12 -0500

    Added optional optimized C library

---
+++ hiroko/lib/buffer.c
@@ -0,0 +1,12 @@
+/*
+    Buffering functions for file management and data piping
+*/
+
+#include <stdio.h>
+#include "evolvement.h"
+
+int main()
+{
+    glass g = {.locations={1, 1, 1}, .people={1, 1, 1}};
+    return 0;
+}
---
+++ hiroko/lib/evolvement.h
@@ -0,0 +1,47 @@
+/*
+    Ultra fast implementation of the evolution algorithm.
+*/
+
+struct petriglass
+{
+    long *locations;
+    int *people;
+};
+
+typedef struct petriglass glass;
+
+
+float calculateStandardDeviation(int population[])
+{
+    return;
+}
+
+float calculatePopulationDistance(int population[])
+{
+    return;
+}
+
+float calculateCountsPerDay(int population[])
+{
+    return;
+}
+
+void mutantCreation(int target[])
+{
+
+}
+
+void fitgeneration(int generation[])
+{
+
+}
+
+void crossIndividuals(int generation[], int fitness[])
+{
+
+}
+
+void purgeGeneration(int generation[], int fitness[])
+{
+
+}
