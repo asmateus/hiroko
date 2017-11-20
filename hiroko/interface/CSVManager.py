b3e8ae619fb67a76dc9aa213c5a54d40b76cb7da
Sorelys Sandoval <sorboy11@gmail.com>
2017-11-19 19:25:24 -0500

    CSVManager fixed for Windows

--- hiroko/interface/CSVManager.py
+++ hiroko/interface/CSVManager.py
@@ -5,6 +5,6 @@ DATA_PTH = os.path.dirname(os.path.abspath(__file__)).split('hiroko')[0] + 'hiro
 
 
 def csvRead(file, filepth=DATA_PTH, delimiter=';'):
-    with open(filepth + file, newline='') as csv_file:
+    with open(filepth + file, newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=delimiter)
         return list(csv_data)[1:]
